3a42029e430ed31ad0eafac5fb7671d556e372ca
Micahhhhhhhh <RobinsonM24@greenhill.org>
2022-01-22 17:13:27 -0600

    HOMEWORK2

---
+++ userimput.py
@@ -0,0 +1,28 @@
+#Micah Robinson
+#1/20/2022
+#imput function, tyoe casting, branching, looping
+import os
+os.system('cls')
+import random
+# print("enter a number from 1-10: ", end=" ")
+
+userInfo=int(input())
+print(userInfo/3)
+useless=int(input())
+print("The number is %.2f " %(useless/3))
+guess=int(input("Please Give me a number"))
+
+# myNumber = 9 
+# myNumber=random.randint(1,10)
+# print("##############################")
+# print("#                            #")
+# print("#         guess a number     #")
+# GameOn=True
+# while(GameOn):
+#     userGuess=int(input("Guess a number from 1-10" ))
+# if myNumber == userGuess: 
+#     print("You guessed it!")
+#     GameOn=False
+# else:
+#     print("good luck next time")
+# print("the number to guess was "+ str(myNumber))
