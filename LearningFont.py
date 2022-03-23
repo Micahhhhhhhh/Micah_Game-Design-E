aa3d9c9bf90cf7d397343b6bc9112a4ae2c48523
Micahhhhhhhh <RobinsonM24@greenhill.org>
2022-03-22 20:45:33 -0500

    INST Code

---
+++ LearningFont.py
@@ -0,0 +1,24 @@
+#MICAH ROBINSON
+# 3/21/2022
+
+import pygame,time
+pygame.init()
+wind=pygame.display.set_mode((700,700))
+pygame.display.set_caption('testing')
+
+TITLE_FNT=pygame.font.SysFont('comicsans', 20)
+MENU_FNT=pygame.font.SysFont('comicsans', 40)
+INST_FNT=pygame.font.SysFont('times new roman', 25)
+text=TITLE_FNT.render('HELLO', 1, (255,0,0))
+wind.fill((255,255,255))
+wind.blit(text,(50,50))
+text=INST_FNT.render("Write your instructions", 1,(0,255,0))
+text=INST_FNT.render('Use keys A,W,S,D to move the square, and the arrow keys to move the circle.', 1, (255,0,0))
+text=INST_FNT.render('Chase the square around, and try to cover the square', 1, (255,0,0))
+text=INST_FNT.render('If the square is covered, the circle will then recieve a point, and grow bigger', 1, (255,0,0))
+text=INST_FNT.render('The game will continute until the circle covers the whole screen', 1, (255,0,0))
+wind.blit(text,(220,220))
+pygame.display.update()
+
+
+pygame.time.delay(10000)
--- Sq_Cr_game.py
+++ Sq_Cr_game.py
@@ -15,6 +15,7 @@
 # K_s                   down square
 #initialize pygame
 import os, random, time, pygame
+from sre_constants import JUMP
 #initialize pygame
 pygame.init()
 
@@ -23,7 +24,7 @@ pygame.init()
 WIDTH=700
 HEIGHT=700
 check=True #for the while loop
-move=5 #pixels
+move=10 #pixels
 #square variables
 xs=20
 ys=20
@@ -42,12 +43,30 @@ pygame.display.set_caption('Circle eats Square')
 
 #define colors
 colors={'white':[255,255,255], 'red':[255,0,0], 'aqua':[102,153, 255],
-'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75]}
-
+'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75],'neon green':[127,255,0]}
 #Get colors
-background= colors.get('red')
-sq_color=colors.get('aqua')
-cr_color=colors.get('purple')
+background= colors.get('pink') #background color
+# sq_color=colors.get('aqua') #square color
+#Making random color for square
+
+randColosr=''
+cr_color=colors.get('white') #circle color
+def changeColor():
+    global randColors
+    colorCheck=True
+    while colorCheck:
+        randColors=random.choice(list(colors))
+        if randColors==background:
+            randColors=random.choice(list(colors))
+        else:
+            colorCheck=False
+
+changeColor()
+sq_color=colors.get(randColors)
+
+MAX=10
+JumpCount=MAX
+JUMP=False
 while check:
     screen.fill(background)
     for case in pygame.event.get():
@@ -60,21 +79,38 @@ while check:
         square.x -= move #substract 5 from the x value
     if keys[pygame.K_d] and square.x <WIDTH-wbox:
         square.x += move
-    if keys[pygame.K_w] and square.y >=move:
-        square.y -= move
-    if keys[pygame.K_s] and square.y <HEIGHT-hbox:
-        square.y += move   
+    #Jumping part
+    if not JUMP:
+        if keys[pygame.K_w] and square.y >=move:
+            square.y -= move
+        if keys[pygame.K_s] and square.y <HEIGHT-hbox:
+            square.y += move 
+        # if keys[pygame.K_space]:    
+            JUMP=True  
+    else:
+        if JumpCount >= -MAX:
+            square.y -= JumpCount*abs(JumpCount)/2
+            JumpCount-=1
+        else:
+            JumpCount=MAX
+            JUMP=False
 
     if keys[pygame.K_LEFT]  and xc >=rad:
         xc -= move 
-    if keys[pygame.K_RIGHT] and xc <WIDTH-wbox:
+    if keys[pygame.K_RIGHT] and xc <WIDTH-rad:
         xc += move
     if keys[pygame.K_UP] and yc >=rad:
         yc -= move
-    if keys[pygame.K_DOWN] and yc <HEIGHT-hbox:
+    if keys[pygame.K_DOWN] and yc <HEIGHT-rad:
         yc += move
          
-        
+    checkCollide=square.collidepoint((xc,yc))
+    if checkCollide:
+        square.x=random.randint(wbox, WIDTH-rad)
+        square.y=random.randint(hbox, HEIGHT-rad)
+        changeColor()
+        sq_color=colors.get(randColors)
+        rad += move
     
    
     pygame.draw.rect(screen, sq_color, square)
@@ -83,6 +119,19 @@ while check:
     pygame.display.update()
     pygame.time.delay(10)
 
+#move for Square
+#Square.x - move = move to left
+#Square.x + move = move to right
+#Square.y - move = Up 
+#Square.y + move = down
+
+#move for circle
+#xc - move = left
+#xc + move = right
+#yc - move = Up
+#yc + move= Down 
+
+# x * abs(x)
 
 
 
